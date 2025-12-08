4fa5c0da01fbbd4665afbca18db760b26bb34a82
NikolaySus <gorkunovnm@gmail.com>
2025-12-09 02:42:50 +0300

    prepare chat fix

--- main.py
+++ main.py
@@ -450,6 +450,7 @@ def image_gen(query: str):
         )
 
         image_base64 = response.data[0].b64_json
+        print(f"FUUUUUCKQ:::: {image_base64}")
         result = "Done!"
     except Exception as e:
         result = f"An error occurred during image gen request: {e}"
@@ -1098,7 +1099,6 @@ class LlmServicer(llm_pb2_grpc.LlmServicer):
 
             object_name_2 = str(uuid.uuid4())
             if meta is not None and hasattr(meta, "image_gen") and meta.image_gen.image_base64:
-                print(f"FUUUUCK: {meta.image_gen.image_base64[:420]}")
                 content = [
                     {
                         "type": "text",
