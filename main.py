c5c3676818b0c6ec1648bfeeeb840a795577db5c
NikolaySus <gorkunovnm@gmail.com>
2025-12-09 10:50:16 +0300

    prepare chat fix

--- main.py
+++ main.py
@@ -455,7 +455,7 @@ def image_gen(query: str):
         )
 
         image_base64 = response.data[0].b64_json
-        result = "The image has been generated, everything is fine."
+        result = "The image has been generated, everything is fine. Do not say that you can't generate the image, you have already done this if you see this message."
     except Exception as e:
         result = f"An error occurred during image gen request: {e}"
     return result, llm_pb2.ToolMetadataResponse(
